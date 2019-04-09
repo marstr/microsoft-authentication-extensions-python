a49ea1e324d23649de7fa113d4dc4d2a4aa25b9d
Martin Strobel <marstr@microsoft.com>
2019-04-08 17:27:49 -0700

    Test happy case with both specified and unspecified locations.

--- tests/test_macos_backend.py
+++ tests/test_macos_backend.py
@@ -4,7 +4,15 @@ import uuid
 
 def test_keychain_roundtrip():
     with Keychain() as subject:
+        location, account = "msal_extension_test1", "test_account1"
         want = uuid.uuid4().hex
-        subject.set_generic_password("msal_extension_test", "test_account", want)
-        got = subject.get_generic_password("msal_extension_test", "test_account")
+        subject.set_generic_password(location, account, want)
+        got = subject.get_generic_password(location, account)
+        assert got == want
+
+    with Keychain('~/Library/Keychains/login.keychain-db') as subject:
+        location, account = "msal_extension_test2", "test_account2"
+        want = uuid.uuid4().hex
+        subject.set_generic_password(location, account, want)
+        got = subject.get_generic_password(location, account)
         assert got == want
