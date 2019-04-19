6b7b2ac83793a7accc4f7f02d72192bbe30eb8fa
Martin Strobel <marstr@microsoft.com>
2019-04-19 13:36:00 -0700

    making tweaks

--- src/msal_extensions/token_provider.py
+++ src/msal_extensions/token_provider.py
@@ -4,16 +4,19 @@ from msal.application import PublicClientApplication
 from .token_cache import ProtectedTokenCache
 
 
+class ProviderUnavailableError(ValueError):
+    pass
+
 class TokenProvider(abc.ABC):
     @abc.abstractmethod
     def available(self):
         # type: () -> bool
-        pass
+        raise NotImplementedError()
 
     @abc.abstractmethod
     def get_token(self, scopes=None):
         # type: (*str) -> {str:str}
-        pass
+        raise NotImplementedError()
 
 
 class TokenProviderChain(TokenProvider):
@@ -41,7 +44,8 @@ class SharedTokenCacheProvider(TokenProvider):
         accounts = self._get_accounts()
         if any(accounts):
             active_account = accounts[0]
-        return self._app.acquire_token_silent(scopes=scopes, account=active_account)
+            return self._app.acquire_token_silent(scopes=scopes, account=active_account)
+        raise ProviderUnavailableError()
 
     def _get_accounts(self):
         return self._app.get_accounts(username=self._username)
