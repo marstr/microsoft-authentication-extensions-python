2032fd686dc2e40fb78e6b688a709cacffcfc125
Martin Strobel <marstr@microsoft.com>
2019-04-26 12:26:08 -0700

    Expose default token chain

--- src/msal_extensions/__init__.py
+++ src/msal_extensions/__init__.py
@@ -1,4 +1,4 @@
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
-from .token_provider import TokenProvider, TokenProviderChain, SharedTokenCacheProvider
+from .token_provider import TokenProvider, TokenProviderChain, SharedTokenCacheProvider, DEFAULT_TOKEN_CHAIN
 from .token_cache import ProtectedTokenCache
--- src/msal_extensions/token_provider.py
+++ src/msal_extensions/token_provider.py
@@ -7,6 +7,7 @@ from .token_cache import ProtectedTokenCache
 class ProviderUnavailableError(ValueError):
     pass
 
+
 class TokenProvider(abc.ABC):
     @abc.abstractmethod
     def available(self):
@@ -14,7 +15,7 @@ class TokenProvider(abc.ABC):
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def get_token(self, scopes=None):
+    def get_token(self, scopes=None, username=None):
         # type: (*str) -> {str:str}
         raise NotImplementedError()
 
@@ -26,28 +27,32 @@ class TokenProviderChain(TokenProvider):
     def available(self):
         return any((item for item in self._links if item.available()))
 
-    def get_token(self, scopes=None):
-        return next((item.get_token(scopes=scopes) for item in self._links if item.available()))
+    def get_token(self, scopes=None, username=None):
+        return next((item.get_token(scopes=scopes, username=username) for item in self._links if item.available()))
 
 
 class SharedTokenCacheProvider(TokenProvider):
-    def __init__(self, client_id, username=None, cache_location=None):
+    DEFAULT_CLIENT_ID = '04b07795-8ddb-461a-bbee-02f9e1bf7b46'
+
+    def __init__(self, client_id=None, cache_location=None):
+        client_id = client_id or SharedTokenCacheProvider.DEFAULT_CLIENT_ID
         cache_location = cache_location or os.path.join(os.getenv('LOCALAPPDATA'), 'msal.cache')
         token_cache = ProtectedTokenCache(cache_location=cache_location)
         self._app = PublicClientApplication(client_id=client_id, token_cache=token_cache)
-        self._username = username
 
     def available(self):
         return any(self._get_accounts())
 
-    def get_token(self, scopes=None):
-        accounts = self._get_accounts()
+    def get_token(self, scopes=None, username=None):
+        accounts = self._get_accounts(username=username)
         if any(accounts):
             active_account = accounts[0]
             return self._app.acquire_token_silent(scopes=scopes, account=active_account)
         raise ProviderUnavailableError()
 
-    def _get_accounts(self):
-        return self._app.get_accounts(username=self._username)
+    def _get_accounts(self, username=None):
+        return self._app.get_accounts(username=username)
 
 
+DEFAULT_TOKEN_CHAIN = TokenProviderChain(
+    SharedTokenCacheProvider())
